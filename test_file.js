274bc329a9dfa5e84456e67a1f56f7ae5c6a852a
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2022-10-03 10:27:53 +0100

    git lecture #2

--- test_file.js
+++ test_file.js
@@ -1,10 +1,9 @@
 const name = "Rob";
 const nickname = "Max";
+let surname;
 
-const greeting = function (){
-
-	console.log("hello " + name + ' aka ' + nickname)
-
-}
+const greeting = function () {
+  console.log("hello " + name + " aka " + nickname + "!");
+};
 
 greeting();
